4a2dc704813484982785513aaa17cdcdc072f893
Furukawa Atsushi <atsushifx@gmail.com>
2025-12-24 06:06:22 +0900

    refactor(valueDomain/index): ヘルパーの集約エクスポート用 index.ts を追加
    - valueDomain/index.ts:
      detectValueHelpers のエクスポートを集約し、valueDomain から再利用しやすく整理

---
+++ packages/@aglabo/agla-logger-composer/src/helpers/valueDomain/index.ts
@@ -0,0 +1,10 @@
+// src: packages/@aglabo/agla-logger-composer/src/helpers/valueDomain/index.ts
+// @(#) Value domain helpers - exports aggregation
+//
+// Copyright (c) 2025 atsushifx <https://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Value classification helpers
+export { detectValueCategory, detectValueKind } from './detectValueHelpers.ts';

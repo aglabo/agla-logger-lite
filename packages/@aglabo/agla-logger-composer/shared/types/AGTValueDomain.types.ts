b845dde7c9d8de06c828cb854685cbb17d6433ee
Furukawa Atsushi <atsushifx@gmail.com>
2026-01-10 20:24:11 +0900

    feat(shared/types): add barrel exports and normalize type filenames
    - packages/@shared/agla-shared/shared/types/index.ts:
      - Add barrel file exporting shared log-related types
    
    - packages/@aglabo/agla-logger-composer/shared/types/AGTValueDomain.types.ts:
      - Rename file from AGTValueDomain.ts to follow *.types.ts convention
      - Update header path comment

--- packages/@aglabo/agla-logger-composer/shared/types/AGTValueDomain.types.ts
+++ packages/@aglabo/agla-logger-composer/shared/types/AGTValueDomain.types.ts
@@ -1,4 +1,4 @@
-// src: packages/@aglabo/agla-logger-composer/shared/types/AGTValueDomain.ts
+// src: packages/@aglabo/agla-logger-composer/shared/types/AGTValueDomain.types.ts
 // @(#) Value domain type definitions for value classification
 //
 // Copyright (c) 2025 atsushifx <https://github.com/atsushifx>
---
+++ packages/@shared/agla-shared/shared/types/index.ts
@@ -0,0 +1,10 @@
+// src: packages/@shared/agla-shared/shared/types/index
+// @(#) logger lite type defined barrel file
+//
+// Copyright (c) 2025 atsushifx <https://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export * from './AGTLogLevel.types';
+export * from './AGTLoggerMessage.types';

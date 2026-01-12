2f835bdfe9b7cfbcf02a21aab8e595cdec53e7a0
Furukawa Atsushi <atsushifx@gmail.com>
2026-01-12 14:16:39 +0900

    chore(logger-utils): add placeholder src entrypoint
    - src/index.ts:
      ダミーエントリポイント `{}`` を追加

---
+++ packages/@aglabo/agla-logger-utils/src/index.ts
@@ -0,0 +1,10 @@
+// src: ./.gitignore
+// @(//) : ignore rules for @aglabo/agla-logger-composer
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// dummy: for build, eslint, ...
+export {};
